400d20fd099df1a51e3c237ded80ec3bee1956ed
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-03-30 20:13:29 +0000

    Finished Lab 17 Extra Challenge

--- iftest/condition03.py
+++ iftest/condition03.py
@@ -6,7 +6,9 @@
 hostname = input("What value should we set for hostname?")
 
 ## use the lower method to test if input value matches expected value
-if hostname.lower() == "mtg":
+if hostname == False:
+    print("You didn't type anything in, dummy.")
+elif hostname.lower() == "mtg":
     print("The hostname was found to be mtg")
     print("hostname matches expected config")
 
