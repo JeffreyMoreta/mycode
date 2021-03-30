7e5557ebfdf9c7ba2348e750a959faef40eaaa82
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-03-30 20:28:17 +0000

    Fixed error with Lab 18 Challenge

--- iftest2/ipask2.py
+++ iftest2/ipask2.py
@@ -3,11 +3,11 @@
 import ipaddress
 
 ipchk = input("Apply an IP address: ") # this line now prompts the user for input
-
-if ipaddress.ip_address(ipchk):
-   print("This is a real IP address")
-else:
-   print("Not a valid IP address")
+try:
+    if ipaddress.ip_address(ipchk):
+        print("This is a real IP address")
+except:
+        print("Not a valid IP address")
 # if user set IP of gateway
 if ipchk == "192.168.70.1":
    print("Looks like the IP address of the Gateway was set: " + ipchk + " This is not recommended.")
