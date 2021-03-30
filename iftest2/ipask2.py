1c2661dde305c63e796350fa2c40ed4625886f64
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-03-30 20:23:40 +0000

    Completed Lab 18 Challenge

--- iftest2/ipask2.py
+++ iftest2/ipask2.py
@@ -1,6 +1,13 @@
 #!/usr/bin/env python3
+# importing ipaddress inorder to properly valid ip address
+import ipaddress
+
 ipchk = input("Apply an IP address: ") # this line now prompts the user for input
 
+if ipaddress.ip_address(ipchk):
+   print("This is a real IP address")
+else:
+   print("Not a valid IP address")
 # if user set IP of gateway
 if ipchk == "192.168.70.1":
    print("Looks like the IP address of the Gateway was set: " + ipchk + " This is not recommended.")
