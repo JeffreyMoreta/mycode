32b0ecc706011282354f83ff062def586fed137d
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-03-30 19:24:58 +0000

    renaming and moving files

---
+++ moveplease01.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+import shutil
+import os
+
+os.chdir('/home/student/mycode/')
+shutil.move('raynor.obj', 'ceph_storage/')
+
+xname = input('What is the new name for kerrigan.obj?')
+shutil.move('ceph_storage/kerrigan.obj', 'cepg_storage/' + xname)
