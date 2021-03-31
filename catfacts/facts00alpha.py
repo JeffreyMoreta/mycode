5c0c24507d593017185ead9e4fcaffa398b2b313
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-03-31 19:45:16 +0000

    Completed lab 38 + modified facts03 for challenges

---
+++ catfacts/facts00alpha.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+"""Alta3 Research | Author: RZFeeser@alta3.com"""
+
+# imports always go at the top of your code
+import requests
+
+def main():
+    """Run time code"""
+    # create r, which is our request object
+    r = requests.get("https://cat-fact.herokuapp.com/facts")
+
+    # display the methods available to our new object
+    print( dir(r) )
+main()
---
+++ catfacts/facts01.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Alta3 Research | Author: RZFeeser@alta3.com"""
+
+# imports always go at the top of your code
+import requests
+
+def main():
+    """Run time code"""
+    # create r, which is our request object
+    r = requests.get("https://cat-fact.herokuapp.com/facts")
+
+    # the .json() method will dump a JSON string into Pythonic data structures. COOL!
+    # This is much easier than using the urllib.request library
+    print(r.json())
+main()
