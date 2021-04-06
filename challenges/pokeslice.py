435e5044d1c0274d6fa91611b73f2cb544ad17dc
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-04-06 11:27:02 -0400

    "Pushing poke slice challenge"

--- challenges/pokeslice.py
+++ challenges/pokeslice.py
@@ -2,11 +2,11 @@
 
 # imports always go at the top of your code
 import requests
+import argparse
 
 
 def main():
-    pokemon = input("Which pokemon would you like to look up?").lower()
-    pokeapi = requests.get("https://pokeapi.co/api/v2/pokemon/" + pokemon).json()
+    pokeapi = requests.get("https://pokeapi.co/api/v2/pokemon/{args.poke}").json()
 
     # Print front_default
     print("front_default url:", pokeapi.get('sprites').get('front_default'))
@@ -21,4 +21,7 @@ def main():
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--poke', help='Provide the name of a Pokemon (or its PokeDex number)').lower()
+    args = parser.parse_args()
     main()
