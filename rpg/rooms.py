46b68e59da90bfc90babb021c9b0f1339a59481c
JeffreyMoreta <jeffreymoreta@gmail.com>
2021-04-08 11:10:02 -0400

    "Pushing rpg"

--- rpg/rooms.py
+++ rpg/rooms.py
@@ -1,41 +1,26 @@
+# A dictionary linking a room to other rooms
+
 rooms = {
-    'HALL': {
-        'south': 'KITCHEN',
-        'east': 'DINING ROOM',
-        'item': 'sword',
-        'item_status': ' inside of a display case. It is unlocked',
-        'randenc': '20',
-        'desc': 'You are in the hall of a large, decrepit house. The walls are blackened from some ancient fire. You get the feeling you are being watched. To the east is a dusty dining room. South is the kitchen... something is moving there.'
+    'Hall': {
+        'south': 'Kitchen',
+        'east': 'Dining Room',
+        'item': 'key'
     },
-    'KITCHEN': {
-        'north': 'HALL',
-        'randenc': '0',
-        'down': 'BASEMENT',
-        'desc': 'You are in what was once a kitchen. Nests made of human bones are draped across every countertop. There is a large hole in the floor. Where it leads you have no idea.'
+    'Kitchen': {
+        'north': 'Hall',
+        'item': 'monster',
     },
-    'BASEMENT': {
-        'spell': 'fireball',
-        'desc': 'You are in a stinking basement with an earthen floor. You can\'t even see your hand in front of your face. You are likely to be eaten by a grue.',
-        'randenc': '0',
-        'up': 'KITCHEN',
-    },
-    'DINING ROOM': {
-        'west': 'HALL',
-        'south': 'GARDEN',
-        'north': 'PANTRY',
-        'desc': 'You are in the dining room. The table is set for an elegant party but is covered a blanket of dust. Sleeping bats cling to the chandelier. North is a dark pantry. South lies the garden. West returns to the hall.',
-        'randenc': '0',
+    'Dining Room': {
+        'west': 'Hall',
+        'south': 'Garden',
         'item': 'potion',
-        'item_status': ' hiding among the bottles of wine. It is cherry red in color'
+        'north': 'Pantry',
     },
-    'GARDEN': {
-        'north': 'DINING ROOM',
-        'spell': 'fireball',
-        'randenc': '0',
+    'Garden': {
+        'north': 'Dining Room'
     },
-    'PANTRY': {
-        'south': 'DINING ROOM',
-        'randenc': '0',
-        'item': 'cookie'
+    'Pantry': {
+        'south': 'Dining Room',
+        'item': 'cookie',
     }
 }
